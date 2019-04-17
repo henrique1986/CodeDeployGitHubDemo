51411c7ef0bfab46b9b78522b1bb35b22eaa414c
henrique1986 <44970261+henrique1986@users.noreply.github.com>
2019-04-17 21:58:09 +0100

    Update instance.tf

--- terraform_website/instance.tf
+++ terraform_website/instance.tf
@@ -1,11 +1,11 @@
 provider "aws" {
  access_key	= "${var.aws_access_key_id}"
  secret_key     = "${var.aws_secret_access_key_id}"
- region         = "us-east-1"
+ region         = "${var.aws_region}"
 }
 
 resource "aws_instance" "test_ubuntu" {
-  ami           = ""${lookup(var.AMIS, var.AWS_REGION)}""
+  ami           = ""${lookup(var.AMIS, var.aws_region)}""
   instance_type = "t2.micro"
   key_name      = "${var.test_key_pair_name}"
   
