8bcdce1ec3f7f530c49a8d98af7828ca8fbe2be1
henrique1986 <44970261+henrique1986@users.noreply.github.com>
2019-04-17 21:59:45 +0100

    Update vars.tf

--- terraform_website/vars.tf
+++ terraform_website/vars.tf
@@ -3,3 +3,15 @@ variable "test_key_pair_name" {}
 variable "aws_access_key_id" {}
 
 variable "aws_secret_access_key_id" {}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-06b94666"
+    eu-west-1 = "ami-0d729a60"
+  }
+}
