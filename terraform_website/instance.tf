88e00df9d56f2aab5d6ae953e1dd2e907b9fdf93
henrique1986 <44970261+henrique1986@users.noreply.github.com>
2019-04-17 21:39:56 +0100

    Update instance.tf

--- terraform_website/instance.tf
+++ terraform_website/instance.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "test_ubuntu" {
-  ami           = "ami-123456890"
+  ami           = "ami-ami-0de53d8956e8dcf80"
   instance_type = "t2.micro"
   key_name      = "test_key_pair_name"
   
