a268ae30e8cfee93a43f14d6c739d9216f097022
root <root@DESKTOP-AKQP4VR.localdomain>
2019-04-17 21:44:25 +0100

    updated

--- terraform_website/instance.tf
+++ terraform_website/instance.tf
@@ -1,13 +1,13 @@
 provider "aws" {
- access_key	    = "aws_access_key_id"
- secret_key     = "aws_secret_access_key_id"
+ access_key	= "${var.aws_access_key_id}"
+ secret_key     = "${var.aws_secret_access_key_id}"
  region         = "us-east-1"
 }
 
 resource "aws_instance" "test_ubuntu" {
   ami           = "ami-123456890"
   instance_type = "t2.micro"
-  key_name      = "test_key_pair_name"
+  key_name      = "${var.test_key_pair_name}"
   
   tags {
     name        = "test_ubuntu"
---
+++ terraform_website/vars.tf
@@ -0,0 +1,5 @@
+variable "test_key_pair_name" {}
+
+variable "aws_access_key_id" {}
+
+variable "aws_secret_access_key_id" {}
