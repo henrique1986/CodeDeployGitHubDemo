493ae3c2bacc953c1bc8190af567eb2cdda83112
henrique1986 <44970261+henrique1986@users.noreply.github.com>
2019-04-17 22:04:34 +0100

    Update autoscaling_policy.tf

--- terraform_website/autoscaling_policy.tf
+++ terraform_website/autoscaling_policy.tf
@@ -1,7 +1,7 @@
 resource "aws_autoscaling_policy" "instances_autoscaling_policy" {
-  name 				            = "instances_autoscaling_policy"
-  scaling_adjustment 		  = 5
-  adjustment_type 		    = "ChangeInCapacity"
-  cooldown 			          = 100
-  autoscaling_group_name 	= "${aws_autoscaling_group.instances_autoscaling_group.name}"
+  name                    = "instances_autoscaling_policy"
+  scaling_adjustment      = 5
+  adjustment_type         = "ChangeInCapacity"
+  cooldown                = 100
+  autoscaling_group_name  = "${aws_autoscaling_group.instances_autoscaling_group.name}"
 }
