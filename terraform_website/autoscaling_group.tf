d50ef3de627eca3088eeeb8f5f585a53cdf71553
henrique1986 <44970261+henrique1986@users.noreply.github.com>
2019-04-18 09:25:02 +0100

    Update autoscaling_group.tf

--- terraform_website/autoscaling_group.tf
+++ terraform_website/autoscaling_group.tf
@@ -1,9 +1,6 @@
 resource "aws_autoscaling_group" "instances_autoscaling_group" {
+  launch_configuration = "${aws_launch_configuration.lc.id}"
   min_size              = 3
   max_size              = 10
   load_balancers        = ["${aws_elb.alb.name}"]
-  launch_configuration {
-    id                  = "${aws_launch_configuration.lc.id}"
-    version             = "$Latest"
-  }
 }
