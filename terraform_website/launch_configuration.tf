8a92353226e828ef9dc179c817c5bf5861ff9839
henrique1986 <44970261+henrique1986@users.noreply.github.com>
2019-04-18 09:29:07 +0100

    Update launch_configuration.tf

--- terraform_website/launch_configuration.tf
+++ terraform_website/launch_configuration.tf
@@ -1,5 +1,6 @@
 resource "aws_launch_configuration" "lc" {
-  image_id 		= "ami-123456789"
-  instance_type 	= "t2.micro"
-  security_groups       = ["${aws_security_group.instance.id}"]
+  image_id        = "${lookup(var.amis,var.aws_region)}"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
+  key_name        = "${var.test_key_pair_name}"
 }
